384f6310fc64b0e306b16358c4f138d743ebd07c
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2024-04-16 12:33:57 -0500

    auth app config

---
+++ apps/auth/config/webpack.common.js
@@ -0,0 +1,18 @@
+module.exports = {
+	module: {
+		rules: [
+			// babel loader
+			{
+				test: /\.m?js$/, // when ever we import a file with extension mjs or js we want it to be processed with babel
+				exclude: /node_modules/, // do not try to run babel against node module files
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-react', '@babel/preset-env'],
+						plugins: ['@babel/plugin-transform-runtime'], // enables diff features such as ES6 syntax and etc
+					},
+				},
+			},
+		],
+	},
+};
---
+++ apps/auth/src/bootstrap.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
+
+// Mount function to start up the app
+
+const mount = (elementHTML, { onNavigate, defaultHistory }) => {
+	const history = defaultHistory || createMemoryHistory();
+
+	if (onNavigate) {
+		history.listen(onNavigate);
+	}
+
+	ReactDOM.render(<App history={history} />, elementHTML);
+
+	return {
+		onParentNavigate({ pathname: nextPathname }) {
+			const { pathname } = history.location;
+			if (pathname !== nextPathname) {
+				history.push(nextPathname);
+			}
+		},
+	};
+};
+
+// If we are in dev and in isolation call mount immediately
+if (process.env.NODE_ENV === 'development') {
+	const devRootElem = document.querySelector('#_auth-dev-root');
+	if (devRootElem) {
+		mount(devRootElem, { defaultHistory: createBrowserHistory() });
+	}
+}
+
+// If we are running through container/host then we should export mount function
+export { mount };
