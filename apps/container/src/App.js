f38d237ea5a43bbbc5dd93c3e9e7d7b0f163026b
Sergii Burgazlieiev <burgazlieiev@gmail.com>
2024-04-16 14:07:54 -0500

    lazy loading perf optimization

--- apps/container/src/App.js
+++ apps/container/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import {
 	StylesProvider,
 	createGenerateClassName,
 } from '@material-ui/core/styles';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
 import Header from './components/Header';
+import Progress from './components/Progress';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 // helps to escape styles collision in prod
 const generateClassName = createGenerateClassName({
@@ -18,10 +20,12 @@ export default () => {
 		<StylesProvider generateClassName={generateClassName}>
 			<BrowserRouter>
 				<Header />
-				<Switch>
-					<Route path='/auth' component={AuthApp} />
-					<Route path='/' component={MarketingApp} />
-				</Switch>
+				<Suspense fallback={<Progress />}>
+					<Switch>
+						<Route path='/auth' component={AuthLazy} />
+						<Route path='/' component={MarketingLazy} />
+					</Switch>
+				</Suspense>
 			</BrowserRouter>
 		</StylesProvider>
 	);
